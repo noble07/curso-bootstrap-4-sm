14a7da9b0c9287e8dffc4ebbc5bf8045c5761cc5
jcmunoz <jcmunoz@smdigital.com.co>
2019-03-14 12:24:32 -0500

    Optimizacion:
    Se carga el font awesome desde CDN para optimizar tiempo de compilación

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,4 @@
 import 'bootstrap';
-import 'bootstrap/js/dist/util';
-import 'bootstrap/js/dist/alert';
-import '@fortawesome/fontawesome-free/js/all'
+// import '@fortawesome/fontawesome-free/js/all';
 
 import '../scss/app.scss';
