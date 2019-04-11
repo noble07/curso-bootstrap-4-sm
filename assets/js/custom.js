fc6321a0c254d556731b64e57953776c25614b17
jcmunoz <jcmunoz@smdigital.com.co>
2019-04-11 14:44:27 -0500

    Maqueta:
    Se maqueta el carrusel de proyectos

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,7 +1,28 @@
 // Custom JS
 var mySwiper = new Swiper('.swiper-container', {
+  slidesPerView: 6,
+  spaceBetween: 0,
+  loop: true,
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: '.fa-arrow-circle-right',
+    prevEl: '.fa-arrow-circle-left',
+  },
+  breakpointsInverse: true,
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    500: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    800: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+    1000: {
+      slidesPerView: 4,
+      spaceBetween: 20,
+    }
   },
 });
